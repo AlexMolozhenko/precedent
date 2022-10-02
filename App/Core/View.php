5d55e329a1203c2d98a32d7c7244adc85cd55c0a
Alexsey Molozhenko <jukov20150@gmail.com>
2022-10-02 22:27:42 +0300

    View.php

---
+++ App/Core/View.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Core;
+
+include_once "..".DIRECTORY_SEPARATOR."config".DIRECTORY_SEPARATOR."configView.php";
+class View
+{
+    /**
+     * properties to store the name of the default  directory
+     * @var string
+     */
+    protected $view_template_dir ;
+    /**
+     * properties to store the name of the default mapping file
+     * @var string
+     */
+    protected $view_template_file ;
+
+    /**
+     * property initialization
+     * @param string $template
+     */
+    public function __construct(string $template)
+    {
+        $this->view_template_dir = $template;
+        $this->view_template_file = $template;
+
+    }
+
+    /**
+     *method for connecting a display file and passing an array of data
+     * @param string $pageTemplate
+     * @param array $data
+     */
+    public function render(string $pageTemplate, array $data = []){
+        extract($data);
+        include_once ROOT_DIRECTORY . DIRECTORY_SEPARATOR . VIEW_DIR . DIRECTORY_SEPARATOR . TEMPLATE_DIR . DIRECTORY_SEPARATOR . $this->view_template_file.'.php';
+    }
+
+
+
+}
