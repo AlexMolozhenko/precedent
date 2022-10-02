02c643d74dd4e183ca8601bf0ed1cc6da44ed358
Alexsey Molozhenko <jukov20150@gmail.com>
2022-10-02 22:28:20 +0300

    UserModel

---
+++ App/Model/UsersModel.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Model;
+
+use Core\ImplementsModel\InterfaceUsers;
+
+use core\ModelAbstract\AbstractModel;
+
+
+class UsersModel extends AbstractModel implements InterfaceUsers
+{
+    /**
+     * get user password for verify
+     * @param $login
+     * @return mixed
+     * @throws \Exception
+     */
+    public function getPassword($login){
+        $sql="SELECT password FROM `users` WHERE login = '$login';";
+
+        $result = $this->db->query($sql);
+        if($this->db->errno !==0 ){
+            throw new \Exception($this->db->errno);
+        }
+        return $result->fetch_array(MYSQLI_ASSOC);
+
+    }
+    /**
+     * get user
+     * @param $login
+     * @return mixed
+     * @throws \Exception
+     */
+    public function getUser($login)
+    {
+//        $sql="SELECT id, login ,role FROM `users` WHERE login = '$login' AND password = '$password' ;";
+
+        $sql="SELECT id, login ,role FROM `users` WHERE login = '$login';";
+//        var_dump($login);
+//        $sql="SELECT * FROM `users` WHERE login='$login' ;";
+
+        $result = $this->db->query($sql);
+        if($this->db->errno !==0 ){
+            throw new \Exception($this->db->errno);
+        }
+        return $result->fetch_array(MYSQLI_ASSOC);
+    }
+
+}
