6657bca2cae67214487bdf7bdf668924d6a407a9
Alexsey Molozhenko <jukov20150@gmail.com>
2022-10-08 22:20:47 +0300

    PrecedentModel - getAllDocument

--- App/Model/PrecedentModel.php
+++ App/Model/PrecedentModel.php
@@ -9,7 +9,8 @@ class PrecedentModel extends AbstractModel implements InterfacePrecedent
 {
     public function getAllDocument()
     {
-        $sql = "SELECT * FROM `t_precedent`;";
+//        $sql = "SELECT * FROM `t_precedent`;";
+        $sql = "SELECT a_id,name_of_record,num_decision,num_litigation,url_doc FROM `t_precedent` ORDER BY a_id DESC;";
         $result = $this->db->query($sql);
         if($this->db->errno !==0 ){
             throw new \Exception($this->db->errno);
