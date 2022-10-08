b336c3e5a71fee07ace2ffb9cfa8b034cef4453c
Alexsey Molozhenko <jukov20150@gmail.com>
2022-10-08 22:23:43 +0300

    JS - editDocument,core,users

--- public_html/JS/core.js
+++ public_html/JS/core.js
@@ -1,3 +1,4 @@
+
 /**
  * form cleaning
  * @param form
@@ -12,4 +13,33 @@ function formReset(form){
  */
 function redirect(url='/'){
     location.href = url;
+}
+
+function IncludePage(responseText){
+    document.getElementById('main').innerHTML = responseText;
+}
+
+function mainPage(method,url){
+    let xhr = new XMLHttpRequest();
+    // xhr.open('GET','precedentList/testPage');
+    xhr.open(method,url);
+    xhr.send();
+    xhr.onreadystatechange = function (){
+        if(xhr.readyState === 4 ){
+            if(xhr.status === 200){
+
+                // document.getElementById('main').innerHTML = this.responseText;
+                IncludePage(this.responseText);
+            }
+        }
+    }
+}
+
+
+
+window.onload = function (e){
+
+    mainPage('GET','precedentList/mainPage');
+
+    e.preventDefault();
 }
--- public_html/JS/users.js
+++ public_html/JS/users.js
@@ -1,68 +1,55 @@
-window.onload = function(e){
 
-    /**
-     * LOGIN USER
-     * sending the logging form to the server
-     */
-    let btn_login =   document.getElementById('btn_login');
-    if(btn_login != null){
-        btn_login.onclick = function (e){
-            let form = document.forms.login_Form;
-            let formData = new FormData(form);
-            let method = form.method;
-            let url = form.action;
-            let xhr = new XMLHttpRequest();
-            xhr.open(method,url);
-            xhr.send(formData);
-            xhr.onreadystatechange = function(){
-                if(xhr.readyState===4){
-                    if(xhr.status === 200){
-                        if(this.responseText === 'true'){
-                            redirect('/');
-                        }else{
-                            alert('TODO error login ');
-                        }
 
+/**
+ * LOGIN USER
+ * sending the logging form to the server
+ */
+        function btn_login (){
+        let form = document.forms.login_Form;
+        let formData = new FormData(form);
+        let method = form.method;
+        let url = form.action;
+        let xhr = new XMLHttpRequest();
+        xhr.open(method,url);
+        xhr.send(formData);
+        xhr.onreadystatechange = function(){
+            if(xhr.readyState===4){
+                if(xhr.status === 200){
+                    if(this.responseText === 'true'){
+
+                        mainPage('GET','precedentList/mainPage');
+                    }else{
+                        alert('TODO error login ');
                     }
+
                 }
             }
-            e.preventDefault();
         }
     }
 
 
-    /**
-     * LOGOUT user
-     * sending request for cleaning  client session
-     */
-    let btn_logout = document.getElementById('btn_logout');
-    if(btn_logout != null){
-        btn_logout.onclick = function(e){
-            let xhr = new XMLHttpRequest();
-            xhr.open('GET','users/logout');
-            xhr.send();
-            xhr.onreadystatechange = function(){
-                if(xhr.readyState === 4){
-                    if(xhr.status === 200){
+/**
+ * LOGOUT user
+ * sending request for cleaning  client session
+ */
+function btn_logout(){
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET','users/logout');
+    xhr.send();
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState === 4){
+            if(xhr.status === 200){
 
-                        if(this.responseText === 'true'){
-                            redirect('/');
-                        }else{
-                            alert('TODO problem logout btn')
-                        }
-                    }
+                if(this.responseText === 'true'){
+                    // redirect('/');
+                    mainPage('GET','precedentList/mainPage');
+                }else{
+                    alert('TODO problem logout btn')
                 }
             }
-            e.preventDefault();
         }
     }
-
-
-
-
-    e.preventDefault();
 }
-
 
 
 
