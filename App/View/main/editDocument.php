0d3f2864bb59b089d5200f44b4e878f43a985667
Alexsey Molozhenko <jukov20150@gmail.com>
2022-10-08 22:22:59 +0300

    editDocument.php

---
+++ App/View/main/editDocument.php
@@ -0,0 +1,6 @@
+<nav>
+    <button type="button" name="home" id="btn_main_page" onclick="btn_main_page()">На головну</button>
+</nav>
+<h2>edit document</h2>
+<p>'get document==TODO</p>
+<p><?=$a_id?></p>
