b16fdfb94c2cc78bd312640acfabb9a71316c27f
Alexsey Molozhenko <jukov20150@gmail.com>
2022-10-20 21:47:07 +0300

    precedentList.js - getDecision,editDocument,pageListDocument

--- public_html/JS/precedentList.js
+++ public_html/JS/precedentList.js
@@ -1,24 +1,39 @@
+/**
+ * a function that sends a request to the server to get a specific document and displays it in the editor
+ * @param a_id
+ */
+function getDecision(a_id){
+    let form = new FormData();
+    form.append('a_id',a_id);
 
-// document.getElementById('table_doc').onclick = function (e) {
-//     let button = e.target.closest('.btn_getDocument');
-//     if(button){
-//         alert(button.id);
-//     }
-//
-//
-//     // console.log('wewew');
-//
-//
-//     e.preventDefault();
-// }
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST','precedentList/getDecision',"true");
+    xhr.send(form);
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState === 4 ){
+            if(xhr.status === 200){
 
+                let response = JSON.parse(this.responseText);
 
-// let table_doc = document.getElementById('table_doc');
+                tinymce.init({
 
+                    selector: '#mytextarea',
+
+                    setup: editor => {
+                        editor.on('init', () => {
+                            editor.setContent(response.docText);
+                        });
+                    }
+                });
+
+            }
+        }
+    }
+}
 
 
 /**
- *
+ *a function that sends a request to the server to get all the information about the document and connects the html with the edit form
  * @param a_id
  */
     function editDocument(a_id) {
@@ -26,42 +41,43 @@
         form.append('a_id',a_id);
 
         let xhr = new XMLHttpRequest();
-        xhr.open('POST','precedentList/editDocument');
+        xhr.open('POST','precedentList/editDocument','true');
+        xhr.overrideMimeType("text/html");
         xhr.send(form);
-
 
         xhr.onreadystatechange = function(){
             if(xhr.readyState === 4 ){
                 if(xhr.status === 200){
+                    IncludePage(xhr.response);
 
+                    getDecision(a_id);
+                }
+            }
+        }
+    }
+
+/**
+ *
+ the function receives data from the form about the first and last keys of the array, as well as an action that indicates the sequence of the next page. receives a page with the requested array of documents from the server
+
+ * @param action
+ */
+    function pageListDocument(action){
+        let form = document.forms.keyPrecedentDoc;
+        let formData= new FormData(form);
+        formData.append('action',action)
+        let xhr = new XMLHttpRequest();
+        xhr.open('POST','precedentList/mainPage');
+        xhr.send(formData);
+
+        xhr.onreadystatechange = function(){
+            if(xhr.readyState === 4 ){
+                if(xhr.status === 200){
                     IncludePage(this.responseText);
-
                 }
             }
         }
 
-
-
-
     }
 
 
-
-
-
-// window.onload = function (e){
-//
-//     // let btn_getDocument =   document.getElementById('btn_getDocument');
-//     // if(btn_getDocument != null){
-//     //     btn_getDocument.onclick = function(e) {
-//     //
-//     //         alert('wewe');
-//     //         // console.log('wewew');
-//     //
-//     //
-//     //         e.preventDefault();
-//     //     }
-//     // }
-//
-//     e.preventDefault();
-// }
